793e841fc4b94f872a654693ebf203b9d4465063
Pranav Sindura <pranav.sindura@gmail.com>
2024-04-23 00:03:32 +0530

    upd statusline and add tabline

--- init.lua
+++ init.lua
@@ -912,18 +912,46 @@ require('lazy').setup({
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
       local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
+
+      local activeContent = function()
+        local mode, mode_hl = statusline.section_mode { trunc_width = 800 }
+        local git = statusline.section_git { trunc_width = 75 }
+        local filename = statusline.section_filename { trunc_width = 140 }
+        local fileinfo = statusline.section_fileinfo { trunc_width = 120 }
+        local search = statusline.section_searchcount { trunc_width = 75 }
+
+        return statusline.combine_groups {
+          { hl = mode_hl, strings = { mode } },
+          {
+            hl = 'MiniStatuslineDevinfo',
+            strings = { git },
+          },
+          '%<', -- Mark general truncate point
+          { hl = 'MiniStatuslineFilename', strings = { filename } },
+          '%=', -- End left alignment
+          { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+          {
+            hl = mode_hl,
+            strings = { search },
+          },
+        }
+      end
+
       statusline.setup {
+        -- set use_icons to true if you have a Nerd Font
+        use_icons = vim.g.have_nerd_font,
+        content = {
+          active = activeContent,
+        },
+      }
+
+      require('mini.tabline').setup {
         use_icons = vim.g.have_nerd_font,
       }
 
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
+      vim.keymap.set('n', 'L', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
+      vim.keymap.set('n', 'H', '<cmd>bprev<cr>', { desc = 'Prev Buffer' })
+      vim.keymap.set('n', '<leader>bc', '<cmd>bd<cr>', { desc = '[B]uffer [C]lose' })
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
