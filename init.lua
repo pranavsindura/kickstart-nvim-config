67962718d03a2689ebc9db2cc1aa588abc5f2159
Pranav Sindura <pranav.sindura@gmail.com>
2024-04-25 13:23:59 +0530

    add git, quickfix, tab maps

--- init.lua
+++ init.lua
@@ -178,11 +178,15 @@ vim.keymap.set('v', '<', '<gv', { desc = 'Indent Left' })
 vim.keymap.set('n', ']<space>', 'm`o<esc>``', { desc = 'Put Blank line Below' })
 vim.keymap.set('n', '[<space>', 'm`O<esc>``', { desc = 'Put Blank line Above' })
 
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']t', '<cmd>tabnext<cr>', { desc = 'Next Tab' })
+vim.keymap.set('n', '[t', '<cmd>tabprev<cr>', { desc = 'Previous Tab' })
+vim.keymap.set('n', '<leader>Tc', '<cmd>tabclose<cr>', { desc = 'Close [T]ab [C]urrent' })
+
+vim.keymap.set('n', ']q', '<cmd>cnext<cr>', { desc = 'Next [Q]uickfix Item' })
+vim.keymap.set('n', '[q', '<cmd>cprev<cr>', { desc = 'Previous [Q]uickfix Item' })
+
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
--- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
--- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -359,6 +363,7 @@ require('lazy').setup({
         ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
         ['<leader>l'] = { name = '[L]SP', _ = 'which_key_ignore' },
         ['<leader>S'] = { name = '[S]ession', _ = 'which_key_ignore' },
+        ['<leader>T'] = { name = '[T]abs', _ = 'which_key_ignore' },
       }
 
       -- visual mode
