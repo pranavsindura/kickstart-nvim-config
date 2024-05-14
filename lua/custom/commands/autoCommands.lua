5dc1014455f976eb7bd02b0e4621a11a5e0c529b
Pranav Sindura <pranav.sindura@gmail.com>
2024-05-14 10:17:57 +0530

    add autocommand for reload after lazygit exit

--- lua/custom/commands/autoCommands.lua
+++ lua/custom/commands/autoCommands.lua
@@ -20,3 +20,20 @@ vim.api.nvim_create_autocmd('VimLeavePre', {
     miniSessions.write(vim.fn.fnamemodify(vim.fn.getcwd(), ':t'))
   end,
 })
+
+-- Trigger `autoread` when files changes on disk
+-- https://unix.stackexchange.com/questions/149209/refresh-changed-content-of-file-opened-in-vim/383044#383044
+-- https://vi.stackexchange.com/questions/13692/prevent-focusgained-autocmd-running-in-command-line-editing-mode
+vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI' }, {
+  pattern = '*',
+  group = vim.api.nvim_create_augroup('custom_file_reload', { clear = true }),
+  command = "if mode() !~ '\v(c|r.?|!|t)' && getcmdwintype() == '' | checktime | endif",
+})
+
+-- Notification after file change
+-- https://vi.stackexchange.com/questions/13091/autocmd-event-for-autoread
+vim.api.nvim_create_autocmd({ 'FileChangedShellPost' }, {
+  pattern = '*',
+  group = vim.api.nvim_create_augroup('custom_file_reload_notification', { clear = true }),
+  command = "echohl WarningMsg | echo 'File changed on disk. Buffer reloaded.' | echohl None",
+})
