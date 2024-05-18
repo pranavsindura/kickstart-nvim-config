f05b7156ef8950ea8d74854e244515994097b750
Pranav Sindura <pranav.sindura@gmail.com>
2024-05-18 22:15:16 +0530

    add diffview, change gitsigns mappings

--- lua/kickstart/plugins/init.lua
+++ lua/kickstart/plugins/init.lua
@@ -43,6 +43,7 @@ require('lazy').setup({
   require 'custom.plugins.scope',
   require 'custom.plugins.tabnine-nvim',
   require 'custom.plugins.nvim-surround',
+  require 'custom.plugins.diffview',
   require 'custom.plugins.neogit',
   require 'custom.plugins.oil',
   require 'kickstart.plugins.debug',
