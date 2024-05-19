15e46560fb07abda285e202ba8fe48e6bf06c0da
Pranav Sindura <pranav.sindura@gmail.com>
2024-05-19 22:07:50 +0530

    change fugitive mapping

--- lua/custom/plugins/vim-fugitive.lua
+++ lua/custom/plugins/vim-fugitive.lua
@@ -2,8 +2,6 @@ return {
   'tpope/vim-fugitive',
   event = 'VeryLazy',
   config = function()
-    vim.keymap.set('n', '<leader>gg', function()
-      vim.cmd 'G'
-    end, { desc = 'git Open Fugitive' })
+    vim.keymap.set('n', '<leader>gg', '<cmd>tabnew +G<cr>', { desc = 'git Open Fugitive' })
   end,
 }
