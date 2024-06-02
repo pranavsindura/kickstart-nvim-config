ade5840026da5282b686f2e35194d0c85ea90b31
Pranav Sindura <pranav.sindura@gmail.com>
2024-06-02 14:49:21 +0530

    resize git window

--- lua/custom/plugins/vim-fugitive.lua
+++ lua/custom/plugins/vim-fugitive.lua
@@ -2,6 +2,6 @@ return {
   'tpope/vim-fugitive',
   event = 'VeryLazy',
   config = function()
-    vim.keymap.set('n', '<leader>gg', '<cmd>tabnew +G<cr>', { desc = 'git Open Fugitive' })
+    vim.keymap.set('n', '<leader>gg', '<cmd>tabnew +G<cr><cmd>resize 15<cr>', { desc = 'git Open Fugitive' })
   end,
 }
