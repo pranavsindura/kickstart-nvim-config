b9ba8d9fc67bc99fe53ccfcded05fc7a67171f88
Pranav Sindura <pranav.sindura@gmail.com>
2024-07-14 21:31:53 +0530

    update better escape binds

--- lua/custom/plugins/better-escape.lua
+++ lua/custom/plugins/better-escape.lua
@@ -1,5 +1,16 @@
 return {
   'max397574/better-escape.nvim',
   event = 'InsertCharPre',
-  opts = { timeout = 300 },
+  opts = {
+    timeout = 300,
+    default_mappings = false,
+    mappings = {
+      i = {
+        j = {
+          k = '<Esc>',
+          j = '<Esc>',
+        },
+      },
+    },
+  },
 }
