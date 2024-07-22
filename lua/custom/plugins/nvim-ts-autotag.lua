409884b223e842a835254ab82383e719f43fb3e0
Pranav Sindura <pranav.sindura@gmail.com>
2024-07-22 21:00:06 +0530

    fix autotag not working

--- lua/custom/plugins/nvim-ts-autotag.lua
+++ lua/custom/plugins/nvim-ts-autotag.lua
@@ -1,5 +1,23 @@
 return {
   'windwp/nvim-ts-autotag',
-  event = 'VeryLazy',
+  event = 'BufReadPre',
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  config = function()
+    require('nvim-ts-autotag').setup {
+      opts = {
+        -- Defaults
+        enable_close = true, -- Auto close tags
+        enable_rename = true, -- Auto rename pairs of tags
+        enable_close_on_slash = true, -- Auto close on trailing </
+      },
+      -- Also override individual filetype configs, these take priority.
+      -- Empty by default, useful if one of the "opts" global settings
+      -- doesn't work well in a specific filetype
+      per_filetype = {
+        -- ['html'] = {
+        -- enable_close = false,
+        -- },
+      },
+    }
+  end,
 }
