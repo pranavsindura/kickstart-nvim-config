6c8c8f7ecd5901566a73d86f0e35be269db559e4
Pranav Sindura <pranav.sindura@gmail.com>
2024-08-03 10:27:30 +0530

    add obsidian plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -21,6 +21,7 @@ require('lazy').setup({
   require 'custom.plugins.gruvbox',
   require 'custom.plugins.sonokai',
   require 'custom.plugins.lush',
+  require 'custom.plugins.obsidian',
   -- require 'custom.plugins.vaporeon',
   require 'custom.plugins.vim-sleuth',
   require 'custom.plugins.Comment',
